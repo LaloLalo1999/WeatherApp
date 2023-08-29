7fa502fd8669c9d8e4f3cb7cb43f8ace83ddedf3
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-08-29 15:36:50 -0600

    Divided app into components

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
+import UpcomingWeather from "./src/components/UpcomingWeather";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text>Current Weather</Text>
-      </View>
-    </SafeAreaView>
-    
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1
-  },
   container: {
-    backgroundColor: "pink",
     flex: 1
   }
 })
