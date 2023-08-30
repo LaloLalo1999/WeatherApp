2841cde765273b25bbe17a3a4cf699983f742d36
LaloLalo1999 <eduardo.lalo1999@gmail.com>
2023-08-29 22:31:13 -0600

    Refactoring

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -4,11 +4,10 @@ import {
   StyleSheet,
   Text,
   FlatList,
-  View,
   StatusBar,
   ImageBackground
 } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import ListItem from '../components/ListItem';
 
 const DATA = [
   {
@@ -49,33 +48,22 @@ const DATA = [
   }
 ];
 
-const Item = (props) => {
-  const { dt_txt, min, max, condition } = props;
-  return (
-    <View style={styles.item}>
-      <Feather name={'sun'} size={50} color={'white'} />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
-    </View>
-  );
-};
-
 const UpcomingWeather = () => {
   const renderItem = ({ item }) => (
-    <Item
+    <ListItem
       condition={item.weather[0].main}
       dt_txt={item.dt_txt}
       min={item.main.temp_min}
       max={item.main.temp_max}
     />
   );
+  const { container, image } = styles;
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={container}>
       <Text>Upcoming Weather</Text>
       <ImageBackground
         source={require('../../assets/upcoming-background.jpg')}
-        style={styles.image}
+        style={image}
       >
         <FlatList
           data={DATA}
@@ -92,24 +80,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
     backgroundColor: 'royalblue'
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    borderWidth: 5,
-    backgroundColor: 'lightblue'
-  },
-  temp: {
-    color: 'white',
-    fontSize: 20
-  },
-  date: {
-    color: 'white',
-    fontSize: 15
   },
   image: {
     flex: 1
